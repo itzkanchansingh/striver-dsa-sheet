f40f00d552389f4d15513b7e974b28aea4743a0d
Kanchan <itzkanchansingh@gmail.com>
2025-07-09 22:58:17 +0530

    Added Basic folder with initial DSA problem

---
+++ Basic/IoOt.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class IoOt{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("Enter value of a:");
+        System.out.println(a);
+        System.out.println(a);
+    }    
+}
